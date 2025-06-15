98cc88db0f465d3883f37c8999fef29da4b76800
binod nagarkoti <binod1365@gmail.com>
2025-06-15 17:24:32 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/common/AnimatedShape.tsx
+++ src/components/common/AnimatedShape.tsx
@@ -1,7 +1,7 @@
-
 'use client';
 
 import * as React from 'react';
+// Imports for R3F, Drei, THREE will be used by AnimatedShapeRenderer
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Edges } from '@react-three/drei';
 import * as THREE from 'three';
@@ -41,11 +41,8 @@ const HexagonInstance: React.FC<HexagonInstanceProps> = ({ position, rotation, s
   );
 };
 
-interface AnimatedShapeProps {
-  // Props if any
-}
-
-const AnimatedShape: React.FC<AnimatedShapeProps> = () => {
+// This component contains the actual R3F scene
+const AnimatedShapeRenderer: React.FC = () => {
   const [hexConfigs, setHexConfigs] = React.useState<Array<{position: [number, number, number], rotation: [number, number, number]}>>([]);
 
   const hexagonShape = React.useMemo(() => {
@@ -65,35 +62,21 @@ const AnimatedShape: React.FC<AnimatedShapeProps> = () => {
 
   React.useEffect(() => {
     const R_HEX = 0.8;
-    const X_UNIT = R_HEX * 1.5; // Horizontal distance between centers of adjacent staggered hexagons
-    const Y_UNIT = R_HEX * Math.sqrt(3) * 0.5; // Vertical distance for half a hexagon height
+    const X_UNIT = R_HEX * 1.5; 
+    const Y_UNIT = R_HEX * Math.sqrt(3) * 0.5; 
 
-    // Predefined positions for 12 hexagons to approximate the image
-    // These are relative [x, y] coordinates; z will be randomized slightly
     const basePositions: Array<[number, number]> = [
-      // Topmost hexagon
       [0, 3 * Y_UNIT],
-      // Second row from top (2 hexagons)
-      [-X_UNIT, 2 * Y_UNIT],
-      [X_UNIT, 2 * Y_UNIT],
-      // Third row from top (3 hexagons, forming the widest part)
-      [-X_UNIT * 1.33, Y_UNIT], // Using 1.33 as a factor for a bit more spread than direct X_UNIT
-      [0, Y_UNIT],
-      [X_UNIT * 1.33, Y_UNIT],
-      // Fourth row (2 hexagons, below the widest part, slightly inset)
-      [-X_UNIT, 0],
-      [X_UNIT, 0],
-      // Fifth row (3 hexagons)
-      [-X_UNIT * 1.33, -Y_UNIT],
-      [0, -Y_UNIT],
-      [X_UNIT * 1.33, -Y_UNIT],
-      // Bottommost hexagon
+      [-X_UNIT, 2 * Y_UNIT], [X_UNIT, 2 * Y_UNIT],
+      [-X_UNIT * 1.33, Y_UNIT], [0, Y_UNIT], [X_UNIT * 1.33, Y_UNIT],
+      [-X_UNIT, 0], [X_UNIT, 0],
+      [-X_UNIT * 1.33, -Y_UNIT], [0, -Y_UNIT], [X_UNIT * 1.33, -Y_UNIT],
       [0, -3 * Y_UNIT],
     ];
     
     const configs = basePositions.map((pos) => {
-      const z = (Math.random() - 0.5) * R_HEX * 0.8; // Slightly randomized depth for "slice" feel
-      const rotZ = (Math.random() - 0.5) * Math.PI / 12; // Slight random tilt
+      const z = (Math.random() - 0.5) * R_HEX * 0.8; 
+      const rotZ = (Math.random() - 0.5) * Math.PI / 12; 
 
       return {
         position: [pos[0], pos[1], z] as [number, number, number],
@@ -111,11 +94,11 @@ const AnimatedShape: React.FC<AnimatedShapeProps> = () => {
   return (
     <div className="w-full h-[300px] md:h-[400px] cursor-grab active:cursor-grabbing">
       {/* @ts-ignore */}
-      <Canvas camera={{ position: [0, 0.5, 7], fov: 60 }}> {/* Adjusted camera Z for better view */}
+      <Canvas camera={{ position: [0, 0.5, 7], fov: 60 }}>
         {/* @ts-ignore */}
-        <ambientLight intensity={1.5} /> {/* Increased ambient light slightly */}
+        <ambientLight intensity={1.5} />
         {/* @ts-ignore */}
-        <directionalLight position={[5, 8, 7]} intensity={2.0} castShadow /> {/* Increased directional light */}
+        <directionalLight position={[5, 8, 7]} intensity={2.0} castShadow />
         <React.Suspense fallback={null}>
           {hexConfigs.map((config, index) => (
             <HexagonInstance
@@ -132,6 +115,23 @@ const AnimatedShape: React.FC<AnimatedShapeProps> = () => {
       </Canvas>
     </div>
   );
+};
+
+
+const AnimatedShape: React.FC = () => {
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    // This will likely be preempted by the loading state in HeroSection's dynamic import,
+    // but serves as an additional safeguard.
+    return <div className="w-full h-[300px] md:h-[400px] flex justify-center items-center">Preparing 3D animation...</div>;
+  }
+
+  return <AnimatedShapeRenderer />;
 };
 
 export default AnimatedShape;
